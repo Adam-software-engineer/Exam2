3d0efc3702275f4d053db568e780759cc201b034
Adam Steinberg <126136311+Adam-software-engineer@users.noreply.github.com>
2024-03-22 19:52:41 -0500

    Update site.js

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 // Exam2 Javascript
 //
-// make a request too https://api.chucknorris.io/jokes/random?category=movie
+// make a request too https://api.chucknorris.io/jokes/random?category=movie using fetch
 // Create an arrow function called “showNextJoke,” that doesn’t have any parameters. It should use the “fetch”
 // Call the “showNextJoke” function as soon as the script loads (do not use an iife). That way a joke is loaded when the page is loaded/refreshed
 // Create an event listener for the button, so that when the button is clicked “showNextJoke” is called, and a new joke is displayed
